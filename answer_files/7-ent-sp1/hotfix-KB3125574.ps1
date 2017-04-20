c890e4523d4989dd62d4c47a945588cd43ff068f
Rob Dyke <robdyke@gmail.com>
2017-04-21 00:17:28 +0100

    Windows 7 64bit SP1 by virtualbox-iso builder
    	new file:   answer_files/7-ent-sp1/Autounattend.xml
    	new file:   answer_files/7-ent-sp1/configure.bat
    	new file:   answer_files/7-ent-sp1/firewall-disable.bat
    	new file:   answer_files/7-ent-sp1/fixnetwork.ps1
    	new file:   answer_files/7-ent-sp1/hotfix-KB3020369.ps1
    	new file:   answer_files/7-ent-sp1/hotfix-KB3102810.ps1
    	new file:   answer_files/7-ent-sp1/hotfix-KB3125574.ps1
    	new file:   answer_files/7-ent-sp1/install-winrm.cmd
    	new file:   answer_files/7-ent-sp1/zz-start-sshd.cmd
    	new file:   windows_7_ent_sp1.json

---
+++ answer_files/7-ent-sp1/hotfix-KB3125574.ps1
@@ -0,0 +1,19 @@
+# Windows 7 - Install SP1 Convenience Rollup
+$nhsbKbName = "Windows 7 KB3125574"
+$nhsbKbUrl = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2016/05/windows6.1-kb3125574-v4-x64_2dafb1d203c8964239af3048b5dd4b1264cd93b9.msu"
+$nhsbKbExe = Join-Path "$($env:windir)\Temp" 'nhsb-windows6.1-kb3125574.msu'
+
+Write-Output "INFO: $nhsbKbName"
+
+if (!(Test-Path $nhsbKbExe)) {
+    Write-Output "INFO: Downloading from $nhsbKbUrl"
+    (New-Object System.Net.WebClient).DownloadFile($nhsbKbUrl, $nhsbKbExe)
+
+    Write-Output "INFO: Installing $nhsbKbName"
+    Start-Process wusa "$nhsbKbExe /quiet" -wait
+    Restart-Computer
+}
+else
+{
+Write-Output "INFO: Skipping $nhsbKbName"
+}
