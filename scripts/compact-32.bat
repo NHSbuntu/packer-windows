b170692397fd316c62b1264b2e079a2b4556bad9
Rob Dyke <robdyke@gmail.com>
2017-04-23 14:31:18 +0100

    Added 32bit version of compact script Uplift ultradefrag version to 7.0.2
    	new file:   scripts/compact-32.bat
    	modified:   scripts/compact.bat

--- scripts/compact-32.bat
+++ scripts/compact-32.bat
@@ -1,13 +1,13 @@
-if not exist "C:\Windows\Temp\7z920-x64.msi" (
-	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
+if not exist "C:\Windows\Temp\7z920.msi" (
+	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920.msi', 'C:\Windows\Temp\7z920.msi')" <NUL
 )
-msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
+msiexec /qb /i C:\Windows\Temp\7z920.msi
 
 if not exist "C:\Windows\Temp\ultradefrag.zip" (
-	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/ultradefrag/stable-release/7.0.2/ultradefrag-portable-7.0.2.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
+	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/ultradefrag/stable-release/7.0.2/ultradefrag-portable-7.0.2.bin.i386.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
 )
 
-if not exist "C:\Windows\Temp\ultradefrag-portable-7.0.2.amd64\udefrag.exe" (
+if not exist "C:\Windows\Temp\ultradefrag-portable-7.0.2.i386\udefrag.exe" (
 	cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\ultradefrag.zip -oC:\Windows\Temp"
 )
 
@@ -19,17 +19,17 @@ if not exist "C:\Windows\Temp\sdelete.exe" (
 	cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\SDelete.zip -oC:\Windows\Temp"
 )
 
-msiexec /qb /x C:\Windows\Temp\7z920-x64.msi
+msiexec /qb /x C:\Windows\Temp\7z920.msi
 
 net stop wuauserv
 rmdir /S /Q C:\Windows\SoftwareDistribution\Download
 mkdir C:\Windows\SoftwareDistribution\Download
 net start wuauserv
 
-cmd /c C:\Windows\Temp\ultradefrag-portable-7.0.2.amd64\udefrag.exe --optimize --repeat C:
+cmd /c C:\Windows\Temp\ultradefrag-portable-7.0.2.i386\udefrag.exe --optimize --repeat C:
 
 cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
-cmd /c C:\Windows\Temp\sdelete64.exe -q -z C:
+cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
 
 wmic computersystem set AutomaticManagedPagefile=False
 wmic pagefileset where name="%SystemDrive%\\pagefile.sys" set InitialSize=1024,MaximumSize=1024
